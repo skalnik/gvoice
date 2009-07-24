2beaa053dc35f176f9163524a06bc720b546f65f
Mike Skalnik <mike.skalnik@gmail.com>
2009-07-23 21:53:42 -0500

    login code added

--- lib/gvoice.rb
+++ lib/gvoice.rb
@@ -12,6 +12,12 @@ module Gvoice
   
   private
   def login
+    @agent = WWW::Mechanize.new
     
+    yml = YAML.load(File.read(File.join('~', '.gvoice')))
+    email, password = yml[:email], yml[:password]
+    home_page = @agent.get('http://google.com/voice')
+    home_page.forms.first.Email, home_page.forms.first.Passwd = email, password
+    home_page.forms.first.submit
   end
 end
