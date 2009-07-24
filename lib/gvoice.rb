cbfebcf09b707162cc79d7145c3ee90e06f76e49
Mike Skalnik <mike.skalnik@gmail.com>
2009-07-23 21:35:11 -0500

    removed testing stuff

---
+++ lib/gvoice.rb
@@ -0,0 +1,17 @@
+$:.unshift File.dirname(__FILE__) 
+
+require 'rubygems'
+require 'mechanize'
+
+module Gvoice
+  extend self
+  
+  def call
+    
+  end
+  
+  private
+  def login
+    
+  end
+end
